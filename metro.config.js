c61fa836939704811275b473ba8174f8fdc8ab62
Thiago Santos <thiagodlsantos9@gmail.com>
2025-10-25 16:22:06 -0300

    chore: add Nativewind compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
-
 config.resolver.sourceExts.push("js", "jsx", "json", "ts", "tsx", "cjs", "mjs");
-
 config.resolver.assetExts.push("glb", "gltf", "png", "jpg");
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: "./global.css" });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
